24b7e6f03edfdd84e4b3fbe69e882e8713a37adf
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-06 23:48:15 +0000

    auroka.assertions.web: Converted assert_has_next to a macro.

--- auroka_assertions_web/src/lib.rs
+++ auroka_assertions_web/src/lib.rs
@@ -1,6 +1,14 @@
 use anyhow::Result;
 use auroka_test_web::{Locator, expect};
 
-pub async fn assert_has_text(locator: Locator, expected: &str) -> Result<()> {
+#[doc(hidden)]
+pub async fn assert_has_text_internal(locator: Locator, expected: &str) -> Result<()> {
   expect(locator).to_have_text(expected).await
+}
+
+#[macro_export]
+macro_rules! assert_has_text {
+  ($locator:expr, $expected:expr) => {
+    $crate::assert_has_text_internal($locator, $expected).await?
+  };
 }
