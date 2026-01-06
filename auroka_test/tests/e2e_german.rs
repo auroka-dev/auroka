48b4b57953f576afce957c407d8e70b3df9402cc
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-06 23:49:01 +0000

    auroka.test: Converted assert_has_next to a macro.

--- auroka_test/tests/e2e_german.rs
+++ auroka_test/tests/e2e_german.rs
@@ -4,11 +4,10 @@ use auroka_test::web::{assert_has_text, with_page};
 #[auroka_test]
 async fn loads_home_in_german() -> anyhow::Result<()> {
   with_page("/de", |page| async move {
-    assert_has_text(
+    assert_has_text!(
       page.locator("footer .socials"),
-      "Folgen Sie uns in den sozialen Medien:",
-    )
-    .await?;
+      "Folgen Sie uns in den sozialen Medien:"
+    );
     Ok(())
   })
   .await
