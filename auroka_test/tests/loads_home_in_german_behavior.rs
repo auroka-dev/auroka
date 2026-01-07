139cc40111e835c39add7350e40147cd2d0f8883
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-07 00:26:03 +0000

    auroka.test: Fixed behavior name.

--- auroka_test/tests/loads_home_in_german_behavior.rs
+++ auroka_test/tests/loads_home_in_german_behavior.rs
@@ -2,7 +2,7 @@ use auroka_test::auroka_test;
 use auroka_test::web::{assert_has_text, with_page};
 
 #[auroka_test]
-async fn loads_home_in_german() -> anyhow::Result<()> {
+async fn loads_home_in_german_behavior() -> anyhow::Result<()> {
   with_page!("/de", |page| {
     assert_has_text!(
       page.locator("footer .socials"),
