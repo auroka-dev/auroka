929eabf9fa04d2c6b8ab6a9ada0c37babd8d4d9e
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-07 00:06:34 +0000

    auroka.test: Updated to use the with_page macro.

--- auroka_test/tests/e2e_german.rs
+++ auroka_test/tests/e2e_german.rs
@@ -3,12 +3,10 @@ use auroka_test::web::{assert_has_text, with_page};
 
 #[auroka_test]
 async fn loads_home_in_german() -> anyhow::Result<()> {
-  with_page("/de", |page| async move {
+  with_page!("/de", |page| {
     assert_has_text!(
       page.locator("footer .socials"),
       "Folgen Sie uns in den sozialen Medien:"
     );
-    Ok(())
   })
-  .await
 }
