54ab3769859d75c3dc0a24bd1bc711dbbaede2fd
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-08 02:46:08 +0000

    auroka.test: Added page namespace export. Updated test to use the with_server macro.

--- auroka_test/src/lib.rs
+++ auroka_test/src/lib.rs
@@ -1,4 +1,8 @@
 pub mod web {
+  pub mod page {
+    pub use auroka_assertions_web_page::*;
+    pub use auroka_test_web_page::*;
+  }
   pub use auroka_assertions_web::*;
   pub use auroka_test_web::*;
 }
--- auroka_test/tests/loads_home_in_german_behavior.rs
+++ auroka_test/tests/loads_home_in_german_behavior.rs
@@ -1,31 +1,18 @@
 use auroka_test::auroka_test;
-use auroka_test::web::{assert_has_text, with_page};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use auroka_test::web::page::{assert_has_text, with_page};
+use auroka_test::web::with_server;
 
 #[auroka_test]
 async fn loads_home_in_german_behavior() -> anyhow::Result<()> {
-  let listener = TcpListener::bind("127.0.0.1:8787").await?;
-
-  let server = tokio::spawn(async move {
-    loop {
-      let (mut socket, _) = listener.accept().await.unwrap();
-      tokio::spawn(async move {
-        let mut buf = [0; 1024];
-        let _ = socket.read(&mut buf).await;
-        let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><footer><div class=\"socials\">Folgen Sie uns in den sozialen Medien:</div></footer></body></html>";
-        let _ = socket.write_all(response.as_bytes()).await;
-      });
+  with_server! {
+    "/de" => "<html><body><footer><div class=\"socials\">Folgen Sie uns in den sozialen Medien:</div></footer></body></html>",
+    |base_url| {
+      with_page!(base_url, "/de", |page| {
+        assert_has_text!(
+          page.locator("footer .socials"),
+          "Folgen Sie uns in den sozialen Medien:"
+        );
+      })?
     }
-  });
-
-  let result = with_page!("/de", |page| {
-    assert_has_text!(
-      page.locator("footer .socials"),
-      "Folgen Sie uns in den sozialen Medien:"
-    );
-  });
-
-  server.abort();
-
-  result
+  }
+}
