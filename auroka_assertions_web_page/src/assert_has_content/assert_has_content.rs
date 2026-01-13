6f2730fb1bfe44d400d07c3d9f2ba4889179b451
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-13 01:57:08 +0000

    auroka_assertions_web_page: Improved formatting.

--- auroka_assertions_web_page/src/assert_has_content/assert_has_content.rs
+++ auroka_assertions_web_page/src/assert_has_content/assert_has_content.rs
@@ -4,14 +4,7 @@ use auroka_test_web_page::Page;
 #[doc(hidden)]
 pub async fn assert_has_content_internal(page: &Page, expected: &str) -> Result<()> {
   let content = page.content().await?;
-  if content.contains(expected) {
-    Ok(())
-  } else {
-    Err(anyhow!(
-      "Expected page content to contain '{}', but it did not.",
-      expected
-    ))
-  }
+  if content.contains(expected) { Ok(()) } else { Err(anyhow!("Expected page content to contain '{}', but it did not.", expected)) }
 }
 
 #[macro_export]
