5d3b5d5347ed8528ae190040070c35072ac3db15
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-13 14:33:29 +0000

    auroka_test_macro: Refactored.

--- auroka_test_macro/src/expand/expand.rs
+++ auroka_test_macro/src/expand/expand.rs
@@ -9,19 +9,12 @@ pub(crate) fn expand(item: proc_macro2::TokenStream) -> proc_macro2::TokenStream
   };
 
   let fn_name = &input_fn.sig.ident;
-  let fn_body = &input_fn.block;
-  let fn_vis = &input_fn.vis;
-  let fn_ret = &input_fn.sig.output;
   let is_async = input_fn.sig.asyncness.is_some();
-
+  let inner_fn_name = syn::Ident::new(&format!("{}_inner", fn_name), fn_name.span());
   let crate_path = resolve_crate_path();
 
-  let inner_fn_name = syn::Ident::new(&format!("{}_inner", fn_name), fn_name.span());
-
-  let inner_fn = generate_inner_function(is_async, fn_vis, &inner_fn_name, fn_ret, fn_body);
-
-  let test_wrapper = generate_test_wrapper(is_async, fn_vis, fn_name, fn_ret, &inner_fn_name);
-
+  let inner_fn = generate_inner_function(is_async, &input_fn.vis, &inner_fn_name, &input_fn.sig.output, &input_fn.block);
+  let test_wrapper = generate_test_wrapper(is_async, &input_fn.vis, fn_name, &input_fn.sig.output, &inner_fn_name);
   let registry_entry = generate_registry_entry(is_async, &crate_path, fn_name, &inner_fn_name);
 
   quote! {
