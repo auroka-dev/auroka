d82cb6c88ec132120761d351fa830c990d395ab8
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-13 14:39:25 +0000

    auroka_test_macro: Refactored.

--- auroka_test_macro/src/expand/expand.rs
+++ auroka_test_macro/src/expand/expand.rs
@@ -9,13 +9,12 @@ pub(crate) fn expand(item: proc_macro2::TokenStream) -> proc_macro2::TokenStream
   };
 
   let fn_name = &input_fn.sig.ident;
-  let is_async = input_fn.sig.asyncness.is_some();
   let inner_fn_name = syn::Ident::new(&format!("{}_inner", fn_name), fn_name.span());
   let crate_path = resolve_crate_path();
 
   let inner_fn = generate_inner_function(&input_fn, &inner_fn_name);
   let test_wrapper = generate_test_wrapper(&input_fn, &inner_fn_name);
-  let registry_entry = generate_registry_entry(is_async, &crate_path, fn_name, &inner_fn_name);
+  let registry_entry = generate_registry_entry(&input_fn, &crate_path, &inner_fn_name);
 
   quote! {
     #inner_fn
--- auroka_test_macro/src/expand/generate_registry_entry.rs
+++ auroka_test_macro/src/expand/generate_registry_entry.rs
@@ -1,7 +1,10 @@
 use quote::quote;
-use syn::Ident;
+use syn::{Ident, ItemFn};
 
-pub(crate) fn generate_registry_entry(is_async: bool, crate_path: &proc_macro2::TokenStream, fn_name: &Ident, inner_fn_name: &Ident) -> proc_macro2::TokenStream {
+pub(crate) fn generate_registry_entry(fn_item: &ItemFn, crate_path: &proc_macro2::TokenStream, inner_fn_name: &Ident) -> proc_macro2::TokenStream {
+  let fn_name = &fn_item.sig.ident;
+  let is_async = fn_item.sig.asyncness.is_some();
+
   let call = if is_async {
     quote! { #inner_fn_name().await }
   } else {
