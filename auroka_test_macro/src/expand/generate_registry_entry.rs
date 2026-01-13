8b7eba35caa95a931a435d38c0591e0c3168096d
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-13 05:53:15 +0000

    auroka_test_macro: Refactored.

--- auroka_test_macro/src/expand/generate_registry_entry.rs
+++ auroka_test_macro/src/expand/generate_registry_entry.rs
@@ -2,37 +2,25 @@ use quote::quote;
 use syn::Ident;
 
 pub(crate) fn generate_registry_entry(is_async: bool, crate_path: &proc_macro2::TokenStream, fn_name: &Ident, inner_fn_name: &Ident) -> proc_macro2::TokenStream {
-  if is_async {
-    quote! {
-      const _: () = {
-        #crate_path::inventory::submit! {
-          #crate_path::Test {
-              name: stringify!(#fn_name),
-              test_fn: || {
-                  use #crate_path::TestReturn;
-                  Box::pin(async move {
-                      #inner_fn_name().await.into_result()
-                  })
-              },
-          }
-        }
-      };
-    }
+  let call = if is_async {
+    quote! { #inner_fn_name().await }
   } else {
-    quote! {
-      const _: () = {
-        #crate_path::inventory::submit! {
-          #crate_path::Test {
-              name: stringify!(#fn_name),
-              test_fn: || {
-                  use #crate_path::TestReturn;
-                  Box::pin(async move {
-                      #inner_fn_name().into_result()
-                  })
-              },
-          }
+    quote! { #inner_fn_name() }
+  };
+
+  quote! {
+    const _: () = {
+      #crate_path::inventory::submit! {
+        #crate_path::Test {
+            name: stringify!(#fn_name),
+            test_fn: || {
+                use #crate_path::TestReturn;
+                Box::pin(async move {
+                    #call.into_result()
+                })
+            },
         }
-      };
-    }
+      }
+    };
   }
 }
