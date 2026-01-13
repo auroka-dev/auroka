28a19ca45f236c9ec7429cd586aa5d9e3d914825
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-13 14:45:59 +0000

    auroka_test_macro: Refactored.

--- auroka_test_macro/src/expand/generate_registry_entry.rs
+++ auroka_test_macro/src/expand/generate_registry_entry.rs
@@ -2,10 +2,9 @@ use quote::quote;
 use syn::{Ident, ItemFn};
 
 pub(crate) fn generate_registry_entry(fn_item: &ItemFn, crate_path: &proc_macro2::TokenStream, inner_fn_name: &Ident) -> proc_macro2::TokenStream {
-  let fn_name = &fn_item.sig.ident;
-  let is_async = fn_item.sig.asyncness.is_some();
+  let name = &fn_item.sig.ident;
 
-  let call = if is_async {
+  let call = if fn_item.sig.asyncness.is_some() {
     quote! { #inner_fn_name().await }
   } else {
     quote! { #inner_fn_name() }
@@ -15,7 +14,7 @@ pub(crate) fn generate_registry_entry(fn_item: &ItemFn, crate_path: &proc_macro2
     const _: () = {
       #crate_path::inventory::submit! {
         #crate_path::Test {
-            name: stringify!(#fn_name),
+            name: stringify!(#name),
             test_fn: || {
                 use #crate_path::TestReturn;
                 Box::pin(async move {
--- auroka_test_macro/src/expand/generate_test_wrapper.rs
+++ auroka_test_macro/src/expand/generate_test_wrapper.rs
@@ -2,16 +2,13 @@ use quote::quote;
 use syn::{Ident, ItemFn};
 
 pub(crate) fn generate_test_wrapper(fn_item: &ItemFn, inner_fn_name: &Ident) -> proc_macro2::TokenStream {
-  let is_async = fn_item.sig.asyncness.is_some();
-  let fn_vis = &fn_item.vis;
-  let fn_name = &fn_item.sig.ident;
-  let fn_ret = &fn_item.sig.output;
+  let (vis, name, ret) = (&fn_item.vis, &fn_item.sig.ident, &fn_item.sig.output);
 
-  if is_async {
+  if fn_item.sig.asyncness.is_some() {
     quote! {
       #[cfg(not(target_arch = "wasm32"))]
       #[test]
-      #fn_vis fn #fn_name() #fn_ret {
+      #vis fn #name() #ret {
           tokio::runtime::Builder::new_current_thread()
               .enable_all()
               .build()
@@ -23,7 +20,7 @@ pub(crate) fn generate_test_wrapper(fn_item: &ItemFn, inner_fn_name: &Ident) -> 
     quote! {
       #[cfg(not(target_arch = "wasm32"))]
       #[test]
-      #fn_vis fn #fn_name() #fn_ret {
+      #vis fn #name() #ret {
           #inner_fn_name()
       }
     }
