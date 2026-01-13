e82b023fcc840e0075a4586de8d56cfa72597aca
Alexandre Faria <alexandre.faria@auroka.pt>
2026-01-13 04:25:47 +0000

    auroka_test: Refactored. Optimized behaviors for faster execution.

--- auroka_test/src/lib.rs
+++ auroka_test/src/lib.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "web")]
 pub mod web {
   pub mod page {
     pub use auroka_assertions_web_page::*;
@@ -8,4 +9,5 @@ pub mod web {
 }
 pub use auroka_test_macro::*;
 pub use auroka_test_registry::*;
+#[cfg(feature = "web")]
 pub use web::*;
